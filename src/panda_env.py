e2194d966bb6bd2d51512b0974cc0b207651748d
Karter Krueger <krueger.karter@gmail.com>
2022-04-20 23:53:53 -0400

    Reformatted to clean up. Depth for the RGBD overhead cam is still work in progress.

--- src/panda_env.py
+++ src/panda_env.py
@@ -16,9 +16,10 @@ class PandaEnv(gym.Env):
     def __init__(self):
         self.pyb = p
         p.connect(p.GUI)
-        p.resetDebugVisualizerCamera(cameraDistance=1.5, cameraYaw=0, cameraPitch=-40, cameraTargetPosition=[0.55,-0.35,0.2])
-        self.action_space = spaces.Box(np.array([-1]*5), np.array([1]*5))
-        self.observation_space = spaces.Box(np.array([-1]*5), np.array([1]*5))
+        p.resetDebugVisualizerCamera(cameraDistance=1.5, cameraYaw=0, cameraPitch=-40,
+                                     cameraTargetPosition=[0.55, -0.35, 0.2])
+        self.action_space = spaces.Box(np.array([-1] * 5), np.array([1] * 5))
+        self.observation_space = spaces.Box(np.array([-1] * 5), np.array([1] * 5))
 
     def reset(self, **kwargs):
         p.resetSimulation()
@@ -63,16 +64,18 @@ class PandaEnv(gym.Env):
         newPosition = [currentPosition[0] + dx,
                        currentPosition[1] + dy,
                        currentPosition[2] + dz]
-        jointPoses = p.calculateInverseKinematics(self.pandaUid,11,newPosition, orientation)[:7]
+        jointPoses = p.calculateInverseKinematics(self.pandaUid, 11, newPosition, orientation)[:7]
 
-        p.setJointMotorControlArray(self.pandaUid, list(range(7))+[9,10], p.POSITION_CONTROL, list(jointPoses)+2*[fingers])
+        p.setJointMotorControlArray(self.pandaUid, list(range(7)) + [9, 10], p.POSITION_CONTROL,
+                                    list(jointPoses) + 2 * [fingers])
 
         p.stepSimulation()
 
         state_object, _ = p.getBasePositionAndOrientation(self.objectUid)
         state_robot = p.getLinkState(self.pandaUid, 11)[0]
-        state_fingers = (p.getJointState(self.pandaUid,9)[0], p.getJointState(self.pandaUid, 10)[0])
-        if state_object[2]>0.45:
+        state_fingers = (p.getJointState(self.pandaUid, 9)[0], p.getJointState(self.pandaUid, 10)[0])
+        if state_object[2] > 0.45:
+            # TODO: add out reward function values here
             reward = 1
             done = True
         else:
@@ -94,7 +97,7 @@ class PandaEnv(gym.Env):
         rot_x = np.cos(-np.radians(wrist_cam_angle)) * wrist_cam_offset
         rot_y = np.sin(-np.radians(wrist_cam_angle)) * wrist_cam_offset
 
-        wrist_rgb = self.get_camera_rgb([state_robot[0]+rot_y, state_robot[1]+rot_x, state_robot[2] - .1], .4,
+        wrist_rgb = self.get_camera_rgb([state_robot[0] + rot_y, state_robot[1] + rot_x, state_robot[2] - .1], .4,
                                         wrist_cam_angle, -90, 0)
 
         observation = {'robot_state': robot_state_obs, 'overhead_rgbd': overhead_rgbd, 'wrist_rgb': wrist_rgb}
@@ -105,50 +108,51 @@ class PandaEnv(gym.Env):
 
     def get_camera_rgb(self, position, distance=.7, yaw=90, pitch=-70, roll=0, resolution=(720, 960), rgbd=False):
         view_matrix = p.computeViewMatrixFromYawPitchRoll(cameraTargetPosition=position,
-                                                            distance=distance,
-                                                            yaw=yaw,
-                                                            pitch=pitch,
-                                                            roll=roll,
-                                                            upAxisIndex=2)
+                                                          distance=distance,
+                                                          yaw=yaw,
+                                                          pitch=pitch,
+                                                          roll=roll,
+                                                          upAxisIndex=2)
         proj_matrix = p.computeProjectionMatrixFOV(fov=60,
-                                                     aspect=float(resolution[1]) /resolution[0],
-                                                     nearVal=0.01,
-                                                     farVal=5)
+                                                   aspect=float(resolution[1]) / resolution[0],
+                                                   nearVal=0.01,
+                                                   farVal=1.0)
         ret = p.getCameraImage(width=resolution[1],
-                                              height=resolution[0],
-                                              viewMatrix=view_matrix,
-                                              projectionMatrix=proj_matrix,
-                                              renderer=p.ER_BULLET_HARDWARE_OPENGL)
-        (_, _, px, _, _) = ret
+                               height=resolution[0],
+                               viewMatrix=view_matrix,
+                               projectionMatrix=proj_matrix,
+                               renderer=p.ER_BULLET_HARDWARE_OPENGL)
+        (_, _, img, depth_array, segmentation) = ret
 
-        cam_array = np.array(px)
-        cam_array = np.reshape(cam_array, (resolution[0], resolution[1], 4))
+        img_array = np.array(img, dtype=np.uint8)
+        img_array = np.reshape(img_array, (resolution[0], resolution[1], 4))
 
-        channels = 4 if rgbd else 3
-        frame_array = cam_array[:, :, :channels]
-        frame_array[:, :, :3] = frame_array[:, :, :3].astype(np.uint8)
+        frame_array = img_array[:, :, :3]
 
-        return frame_array
+        if not rgbd:
+            return frame_array
+        else:
+            return frame_array, depth_array
 
     def render(self, mode='human'):
-        view_matrix = p.computeViewMatrixFromYawPitchRoll(cameraTargetPosition=[0.7,0,0.05],
-                                                            distance=.7,
-                                                            yaw=90,
-                                                            pitch=-70,
-                                                            roll=0,
-                                                            upAxisIndex=2)
+        view_matrix = p.computeViewMatrixFromYawPitchRoll(cameraTargetPosition=[0.7, 0, 0.05],
+                                                          distance=.7,
+                                                          yaw=90,
+                                                          pitch=-70,
+                                                          roll=0,
+                                                          upAxisIndex=2)
         proj_matrix = p.computeProjectionMatrixFOV(fov=60,
-                                                     aspect=float(960) /720,
-                                                     nearVal=0.1,
-                                                     farVal=100.0)
+                                                   aspect=float(960) / 720,
+                                                   nearVal=0.1,
+                                                   farVal=100.0)
         (_, _, px, _, _) = p.getCameraImage(width=960,
-                                              height=720,
-                                              viewMatrix=view_matrix,
-                                              projectionMatrix=proj_matrix,
-                                              renderer=p.ER_BULLET_HARDWARE_OPENGL)
+                                            height=720,
+                                            viewMatrix=view_matrix,
+                                            projectionMatrix=proj_matrix,
+                                            renderer=p.ER_BULLET_HARDWARE_OPENGL)
 
         rgb_array = np.array(px, dtype=np.uint8)
-        rgb_array = np.reshape(rgb_array, (720,960, 4))
+        rgb_array = np.reshape(rgb_array, (720, 960, 4))
 
         rgb_array = rgb_array[:, :, 3]
         return rgb_array
